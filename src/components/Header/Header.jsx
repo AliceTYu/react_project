a46ed38775d5edfdaf894e092009660a57939131
AliceTYu <turaeva.alice@gmail.com>
2023-02-23 16:05:49 +0300

    second commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Header.module.css';
 
 const Header = () => {
   return (
-    <header className="header">
+    <header className={s.header}>
         <img src="https://cdn-icons-png.flaticon.com/64/7678/7678144.png" />
     </header>
   );
