7c189d43488dc886ab034ce60c0059de6318baea
AliceTYu <turaeva.alice@gmail.com>
2023-03-05 20:12:31 +0300

    add contex api

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -4,24 +4,30 @@ import {
   updateNewMessageTextCreator,
 } from "../../redux/dialogs-reducer";
 import Dialogs from "./Dialogs";
+import StoreContext from "./../../StoreContext";
 
-const DialogsContainer = (props) => {
-  let state = props.store.getState().dialogsPage;
-
-  let addMessage = () => {
-    props.store.dispatch(addMessageCreator());
-  };
-
-  let onMessageChange = (text) => {
-    props.store.dispatch(updateNewMessageTextCreator(text));
-  };
-
+const DialogsContainer = () => {
   return (
-    <Dialogs
-      addMessage={addMessage}
-      onMessageChange={onMessageChange}
-      dialogsPage={state}
-    />
+    <StoreContext.Consumer>
+      {(store) => {
+        let state = store.getState().dialogsPage;
+
+        let addMessage = () => {
+          store.dispatch(addMessageCreator());
+        };
+
+        let onMessageChange = (text) => {
+          store.dispatch(updateNewMessageTextCreator(text));
+        };
+        return (
+          <Dialogs
+            addMessage={addMessage}
+            onMessageChange={onMessageChange}
+            dialogsPage={state}
+          />
+        );
+      }}
+    </StoreContext.Consumer>
   );
 };
 
